a354b3d067649618d1afaf2e3120c42f1eb35958
rina <katerina5314@mail.ru>
2025-02-03 09:19:25 +0300

    Revert "import files"
    This reverts commit c236eb79ae7e2d14a737d5f96a1aa0b6a6ff69c3.

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-package jm.task.core.jdbc;
-
-public class Main {
-    public static void main(String[] args) {
-        // implement algorithm here
-    }
-}
--- src/util/Util.java
+++
@@ -1,5 +0,0 @@
-package jm.task.core.jdbc.util;
-
-public class Util {
-    // set up a database connection
-}
